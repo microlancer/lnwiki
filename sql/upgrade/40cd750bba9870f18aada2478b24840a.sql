a846c27f0fd53a737a13964aaa2ae26f42a95a15
Tim Horie <tak@thorie.com>
2020-02-14 16:05:51 +0000

    Created table for pages and invoices

--- sql/upgrade/40cd750bba9870f18aada2478b24840a.sql
+++ sql/upgrade/40cd750bba9870f18aada2478b24840a.sql
@@ -13,3 +13,29 @@ ALTER TABLE `sessions` ADD KEY `access` (`access`);
 ALTER TABLE `sessions`
 ADD `activeLock` tinyint(1) NOT NULL DEFAULT '1' AFTER `access`;
 
+create table if not exists `pages` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(32) NOT NULL,
+    `createdTs` timestamp default current_timestamp,
+    `content` text
+   ) engine = InnoDB;
+
+alter table `pages` add key `name` (`name`);
+alter table `pages` add key `createdTs` (`createdTs`);
+
+insert into pages (name, content) values ('', 'Welcome to lnwiki!');
+
+CREATE TABLE IF NOT EXISTS `invoices` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `pageId` bigint(20) NOT NULL,
+  `bolt11` varchar(2048) NOT NULL,
+  `label` varchar(1024) NOT NULL,
+  `status` int (11) NOT NULL,
+  `createdTs` datetime NOT NULL,
+  `updatedTs` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+alter table `invoices` add key `pageId` (`pageId`);
+alter table `invoices` add key `status` (`status`);
+
